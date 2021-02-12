a04712604977807d2108ec9183c223da8eb449f2
Robert Feldhans <rfeldhans@techfak.uni-bielefeld.de>
2021-02-12 15:56:15 +0100

    updated filter script

--- data/twitter/filter.py
+++ data/twitter/filter.py
@@ -23,17 +23,17 @@ def process_tweet(text):
 
     # reject tweets which are....
     if len([word for word in text.split(' ') if 'https://t.co/' not in word]) < 4:  # ...not at least 4 non-link words long,
-        return None
+        return None, None
     if len([char for char in text if char not in string.whitespace and char not in string.punctuation]) < 10:  # ...do only contain spaces and punctuation,
-        return None
+        return None, None
     try:
         if detect(text) != 'en':  # ...are not in english,
-            return None
+            return text, 'no'
     except LangDetectException:  # ... or where the language is not detectable
         print('Language Detect Exception with string: ', text)
-        return None
+        return None, None
 
-    return text
+    return text, 'en'
 
 
 def process_dataset(filename):
@@ -43,6 +43,7 @@ def process_dataset(filename):
             raw_data_.append(line)
 
     processed = []
+    processed_foreign = []
 
     too_few_entries = []  # some rows don't have enough entries... weird
     wrong_order = []  # some rows are not ordered in the right way... weird again, but apparently all are in too_few_entries
@@ -51,28 +52,33 @@ def process_dataset(filename):
         if len(row) < len(raw_data_[0]):
             too_few_entries.append(row)
             continue
-        text = process_tweet(row[2])
+        text, lang = process_tweet(row[2])
         try:
             time = datetime.datetime.fromisoformat(row[0])
         except Exception:
             wrong_order.append(row)
             continue
         if text:
-            processed.append((time, text))
+            if lang == 'en':
+                processed.append((time, text))
+            else:
+                processed_foreign.append((time, text))
 
     print('Number of malformated rows:', len(too_few_entries), '+', len(wrong_order))
-    return processed
+    return processed, processed_foreign
 
 
 print('Processing Biden Dataset...')
-processed_biden = process_dataset('hashtag_joebiden.csv')
+processed_biden, biden_fe = process_dataset('hashtag_joebiden.csv')
 
 print('Processing Trump Dataset...')
-processed_trump = process_dataset('hashtag_donaldtrump.csv')
+processed_trump, trump_fe = process_dataset('hashtag_donaldtrump.csv')
 
 print('Dumping the data...')
 with open('election_dataset.pickle', 'wb') as handle:
     pickle.dump({
         'biden': processed_biden,
-        'trump': processed_trump
+        'trump': processed_trump,
+        'biden_fe': biden_fe,
+        'trump_fe': trump_fe
     }, handle)
